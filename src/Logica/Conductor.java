ad38ef7775b60c626be8e1f0e3ff32a95d85dcfc
Judamayaga <juandavid.amaya@mercadolibre.com.co>
2021-09-13 01:22:41 -0500

    [Feature] Setters and getters for both classes's properties

--- src/Logica/Conductor.java
+++ src/Logica/Conductor.java
@@ -5,6 +5,167 @@
  */
 package Logica;
 
+import java.sql.Timestamp;
+
 public class Conductor {
-    String whoami = "whoami";
+    String identificacion;
+    String nombres;
+    String apellidos;
+    String genero;
+    long telefono;
+    long celular;
+    Timestamp fechaDeNacimiento;
+    String correoElectronico;
+    String numeroLicencia;
+    String categoriaLicencia;
+    String turno;
+    String ciudadDeResidencia;
+    String direccion;
+    String barrio;
+    String estadoCivil;
+    int codigoVehiculoAsignado;
+    
+    // Identificación
+    public String getIdentificacion() {
+        return this.identificacion;
+    }
+    
+    public void setIdentificacion(String identificacion) {
+        this.identificacion = identificacion;
+    }
+    
+    // Nombres
+    public String getNombres() {
+        return this.nombres;
+    }
+    
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+    
+    // Apellidos
+    public String getApellidos() {
+        return this.apellidos;
+    }
+    
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+    
+    // Genero
+    public String getGenero() {
+        return this.genero;
+    }
+    
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+    
+    // Telefono;
+    public long getTelefono() {
+        return this.telefono;
+    }
+    
+    public void setTelefono(long telefono) {
+        this.telefono = telefono;
+    }
+    
+    // Celular;
+    public long getCelular() {
+        return this.celular;
+    }
+    
+    public void setCelular(long celular) {
+        this.celular = celular;
+    }
+    
+    // Fecha De Nacimiento
+    public Timestamp getFechaDeNacimiento() {
+        return this.fechaDeNacimiento;
+    }
+    
+    public void setFechaDeNacimiento(Timestamp fechaDeNacimiento) {
+        this.fechaDeNacimiento = fechaDeNacimiento;
+    }
+    
+    // Correo Electronico
+    public String getCorreoElectronico() {
+        return this.correoElectronico;
+    }
+    
+    public void setCorreoElectronico(String correoElectronico) {
+        this.correoElectronico = correoElectronico;
+    }
+    
+    // Numero Licencia
+    public String getNumeroLicencia() {
+        return this.numeroLicencia;
+    }
+    
+    public void setNumeroLicencia(String numeroLicencia) {
+        this.numeroLicencia = numeroLicencia;
+    }
+    
+    // Categoria Licencia
+    public String getCategoriaLicencia() {
+        return this.categoriaLicencia;
+    }
+    
+    public void setCategoriaLicencia(String categoriaLicencia) {
+        this.categoriaLicencia = categoriaLicencia;
+    }
+    
+    // Turno
+    public String getTurno() {
+        return this.turno;
+    }
+    
+    public void setTurno(String turno) {
+        this.turno = turno;
+    }
+    
+    // Ciudad De Residencia
+    public String getCiudadDeResidencia() {
+        return this.ciudadDeResidencia;
+    }
+    
+    public void setCiudadDeResidencia(String ciudadDeResidencia) {
+        this.ciudadDeResidencia = ciudadDeResidencia;
+    }
+    
+    // Direccion
+    public String getDireccion() {
+        return this.direccion;
+    }
+    
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+    
+    // Barrio
+    public String getBarrio() {
+        return this.barrio;
+    }
+    
+    public void setBarrio(String barrio) {
+        this.barrio = barrio;
+    }
+    
+    // EstadoCivil
+    public String getEstadoCivil() {
+        return this.estadoCivil;
+    }
+    
+    public void setEstadoCivil(String estadoCivil) {
+        this.estadoCivil = estadoCivil;
+    }
+    
+    // Codigo Vehiculo Asignado
+    public int getCodigoVehiculoAsignado() {
+        return this.codigoVehiculoAsignado;
+    }
+    
+    public void setCodigoVehiculoAsignado(int codigoVehiculoAsignado) {
+        this.codigoVehiculoAsignado = codigoVehiculoAsignado;
+    }
 }
--- src/Logica/Vehiculo.java
+++ src/Logica/Vehiculo.java
@@ -4,22 +4,96 @@
  * and open the template in the editor.
  */
 package Logica;
-import java.sql.*;
+import java.sql.Timestamp;
 
 public class Vehiculo {
-    static Connection con;
-    static Conexion cx;
+    // static Connection con;
+    // static Conexion cx;
     
-    public boolean GuardarCliente() {
-        try {
-            cx = new Conexion();
-            con = cx.getConexion();
-            System.out.println(con);
-        } catch(Exception e) {
-            System.out.println(e.getMessage());
-            return false;
-        }
-        
-        return true;
+    private int codigoVehiculo;
+    private String placa;
+    private String tipo;
+    private String marca;
+    private String modelo;
+    private String descripcion;
+    private Timestamp inicioOperacion;
+    private String estadoAsignacion;
+    
+    // Setters y getters para cada propiedad
+    // Código de vehiculo
+    public int getCodigoVehiculo() {
+        return this.codigoVehiculo;
     }
+    
+    public void setCodigoVehiculo(int codigoVehiculo) {
+        this.codigoVehiculo = codigoVehiculo;
+    }
+    
+    // Placa
+    public String getPlaca() {
+        return this.placa;
+    }
+    
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+    
+    // Tipo
+    public String getTipo() {
+        return this.tipo;
+    }
+    
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+    
+    // Marca
+    public String getMarca() {
+        return this.marca;
+    }
+    
+    public void setMarca(String tipo) {
+        this.marca = marca;
+    }
+    
+    // Modelo
+    public String getModelo() {
+        return this.modelo;
+    }
+    
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+    
+    // Descripción
+    public String getDescripcion() {
+        return this.descripcion;
+    }
+    
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+    
+    // Inicio de operación
+    public Timestamp getInicioOperacion() {
+        return this.inicioOperacion;
+    }
+    
+    public void setInicioOperacion(Timestamp inicioOperacion) {
+        this.inicioOperacion = inicioOperacion;
+    }
+    
+    // Estado de asignación
+    public String getEstadoAsignacion() {
+        return this.estadoAsignacion;
+    }
+    
+    public void setEstadoAsignacion(String estadoAsignacion) {
+        this.estadoAsignacion = estadoAsignacion;
+    }
+    
+    // Create Vehiculo
+    // Read Vehiculo
+    // Update Vehiculo
+    // Delete Vehiculo    
 }
